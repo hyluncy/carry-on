74f5f7503a4413f8ed0511e178ecf6d4e2368e47
Lucy Kwak <bkwak17@gmail.com>
2025-01-23 22:03:00 -0500

    Focus only on signup route

--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,6 @@
 const express = require('express'); 
 const router = express.Router(); 
-const {
-    registerUser, 
-    loginUser, 
-    findUser,
-} = require('../controllers/user'); 
+const { registerUser } = require('../controllers/user'); 
 
 router.post('/signup', registerUser); 
-router.post('/login', loginUser); 
-
 module.exports = router; 
